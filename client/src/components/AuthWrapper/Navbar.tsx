7d6941b6cdc907f3985b94d85aace6e732e2f23b
NZYM <enzsama.dev@gmail.com>
2025-12-12 19:27:23 +0100

    Add nav links for xs screen sizes

--- client/src/components/AuthWrapper/Navbar.tsx
+++ client/src/components/AuthWrapper/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import UserNav from "./UserNav";
 import { Button } from "../ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Menu } from "lucide-react";
 
 const Navbar = ({ hasSession }: { hasSession: boolean }) => (
   <nav className="border-b border-gray-200">
@@ -8,12 +10,12 @@ const Navbar = ({ hasSession }: { hasSession: boolean }) => (
       <div className="max-w-5xl w-full px-6">
         <div className="flex items-center justify-between w-full py-6">
           <Link to={hasSession ? "/dashboard" : "/"}>
-            <p className="font-semibold text-3xl text-purple-600">Routana</p>
+            <p className="font-semibold text-2xl sm:text-3xl text-purple-600">Routana</p>
           </Link>
           {hasSession
             ? <UserNav />
             : <>
-              <div className="hidden md:flex items-center gap-x-6">
+              <div className="hidden sm:flex items-center gap-x-6">
                 <Link to={"login"}>
                   <Button variant={"link"} className="text-base cursor-pointer font-semibold">
                     Log in
@@ -28,7 +30,25 @@ const Navbar = ({ hasSession }: { hasSession: boolean }) => (
                 </Link>
               </div>
 
-              {/* MOBILE NAV BUTTONS GOES HERE */}
+              <div className="block sm:hidden">
+                <DropdownMenu>
+                  <DropdownMenuTrigger>
+                    <Menu />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="rounded-md p-2 flex flex-col gap-4" align="end">
+                    <Link to={"/login"}>
+                      <DropdownMenuItem className="font-semibold">
+                        Log in
+                      </DropdownMenuItem>
+                    </Link>
+                    <Link to={"/signup"}>
+                      <DropdownMenuItem className="font-semibold text-purple-600">
+                        Get started
+                      </DropdownMenuItem>
+                    </Link>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
             </>
           }
         </div>
